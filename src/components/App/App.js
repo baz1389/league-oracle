ebb026b530c6069f5a5fbc14e6e72a6c78f31557
Ben Spencer <benjamin.a.spencer@gmail.com>
2018-11-21 10:13:43 -0500

    refactors file structure

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styles from './styles.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.App}>
+        Hello
+      </div>
+    );
+  }
+}
+
+export default App;
